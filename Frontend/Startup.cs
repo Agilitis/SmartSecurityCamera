8becc94e4d9699e9e6e29fc9303c8a4cddc879c9
Kálmán Zsolt <zsolter14@gmail.com>
2020-02-29 09:44:52 +0100

    Use developer exception page in production too

--- Frontend/Startup.cs
+++ Frontend/Startup.cs
@@ -38,6 +38,7 @@ namespace Frontend
             }
             else
             {
+                app.UseDeveloperExceptionPage();
                 app.UseExceptionHandler("/Error");
                 app.UseHsts();
             }
